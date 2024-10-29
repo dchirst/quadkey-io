df99878a55ef21de24f7ad6d2a49d345696fd7f9
Dan Hirst <dchirst0@gmail.com>
2024-10-29 21:40:05 +0100

    linting

--- svelte.config.js
+++ svelte.config.js
@@ -3,22 +3,22 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),
 
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: false,
-      strict: true
-    })
-  },
-  vite: {
-    define: {
-      'process.env.VITE_MAPTILER_API_KEY': JSON.stringify(process.env.VITE_MAPTILER_API_KEY)
-    }
-  }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	},
+	vite: {
+		define: {
+			'process.env.VITE_MAPTILER_API_KEY': JSON.stringify(process.env.VITE_MAPTILER_API_KEY)
+		}
+	}
 };
 
 export default config;
