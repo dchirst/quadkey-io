6cc2ce4ee08b34385e102598f514ca745467762c
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2024-11-02 16:47:27 +0100

    Cleanup and include name of quadkey when highlighting (#9)
    * include highlighted quadkey names
    
    * version bump

--- RELEASE.md
+++ RELEASE.md
@@ -1,5 +1,11 @@
+# Release v0.0.9
+
+Add text to highlighted quadkeys
+
+---
+
 # Release v0.0.8
 
 Add github actions to publish tags
 
---------------------
+---
--- src/stores.ts
+++ src/stores.ts
@@ -1,4 +1,7 @@
 import { writable } from 'svelte/store';
 
+// A store of the active quadkeys
 export const quadkeys = writable<string[]>([]);
+
+// Whether to support selecting multiple quadkeys at once on the map.
 export const multiSelect = writable<boolean>(false);
