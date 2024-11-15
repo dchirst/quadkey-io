d6c1ccf404a7744f5ed817567dde65fd526102b1
Dan Hirst <dchirst0@gmail.com>
2024-11-15 23:22:30 +0100

    add dark mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,6 +17,8 @@
 	let map: maplibregl.Map;
 	let mapContainer: HTMLDivElement;
 	let zoom: number;
+	let dark;
+
 
 	// When the list of quadkeys changes, highlight them on the map
 
@@ -38,11 +40,20 @@
 
 		zoom = initialState.zoom;
 
+        if (localStorage.theme === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            dark = true
+        } else {
+            dark = false
+        }
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+            dark = event.matches;
+        });
+
 		map = new maplibregl.Map({
 			container: mapContainer,
 			style: `https://tiles.openfreemap.org/styles/positron`,
 			center: [initialState.lng, initialState.lat],
-			zoom: initialState.zoom
+			zoom: initialState.zoom,
 		});
 
 		map.on('load', () => {
@@ -83,5 +94,11 @@
 
 <Panel />
 <div class="relative w-full" style="height: 100vh">
-	<div class="absolute h-full w-full" bind:this={mapContainer}></div>
+	<div class="absolute h-full w-full {dark ? 'mapDark' : ''}" bind:this={mapContainer}></div>
 </div>
+
+<style>
+	.mapDark {
+			filter: brightness(0.6) invert(1) contrast(3) hue-rotate(200deg) saturate(0.3) brightness(0.7);
+	}
+</style>
